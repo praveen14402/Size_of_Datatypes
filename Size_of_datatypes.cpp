95dfcb3afe44cf891435cee898489d43a7947e54
Praveen <95998093+praveen14402@users.noreply.github.com>
2022-04-13 18:11:05 +0530

    Create Size_of_datatypes.cpp

---
+++ Size_of_datatypes.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+ int main(){
+	cout<<"Size of char  "<<sizeof(char)<<" byte"<<endl;
+	cout<<"size if int  "<<sizeof(int)<<" byte"<<endl;
+	cout<<"size of float "<<sizeof(float)<<" byte"<<endl;
+	cout<<"size of double "<<sizeof(double)<<" byte"<<endl;
+
+	return 0;
+}
