019a311f89c911d0dd3777de139b59a41c679a89
Praveen <95998093+praveen14402@users.noreply.github.com>
2022-04-13 18:14:23 +0530

    Create Product.cpp

---
+++ Product.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+
+using namespace std;
+
+
+int  main(){
+	double u_input1,u_input2,product;
+	// take the input from user
+	cout<<"Enter input 1  :  ";
+	cin>>u_input1;
+	cout<<"Enter input 2  :  ";
+	cin>>u_input2;
+	
+	
+    product =u_input1*u_input2;
+    cout<<"The Product of two numbers is :"<<product;
+    return 0;
+	
+}
